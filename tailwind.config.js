4fdaaa4ead1f99ad72e02c16e921390b9c6ff4fb
eonadela-ca <pjaynadela@gmail.com>
2023-06-29 22:18:28 -0700

    change color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
       montserrat: ['Montserrat', 'sans-serif'],
       roboto: ['Roboto', 'serif'],
     },
+    colors: {
+      transparent: 'transparent',
+      white: '#FFFFFF',
+      black: '#000000',
+      primary: '#6DC0B3',
+      secondary: '#E3BB7A',
+    },
     extend: {},
   },
   plugins: [],
